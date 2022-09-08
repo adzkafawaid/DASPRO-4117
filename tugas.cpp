f92217ee75acdccb0ae98c0ff25301fe47e389c7
adzkafawaid <112879082+adzkafawaid@users.noreply.github.com>
2022-09-08 19:48:35 +0700

    Add files via upload

---
+++ tugas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< "==================================================" << endl;
+    cout<< "|| \t\t HELLO EVERYONE ! \t\t||" << endl;
+    cout<< "|| NAMA\t\t : Mohamad Adzka Fawaid\t\t||"<< endl;
+    cout<< "|| NIM\t\t : A11.2022.14656\t\t\||"<< endl;
+    cout<< "|| Jenis kelamin : LAKI LAKI\t\t\t||"<< endl;
+    cout<< "|| Asal Sekolah  : SMAN 1 KRAMAT\t\t||"<< endl;
+    cout<< "=================================================="<< endl;
+    return 0;
+}
