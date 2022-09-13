35c9a3df30d732dd2c8e92c3bf51b3465e16aab1
adzkafawaid <112879082+adzkafawaid@users.noreply.github.com>
2022-09-13 12:02:37 +0700

    Add files via upload
    MOHAMAD ADZKA FAWAID
    A11.2022.14656

---
+++ pertemuan3.cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //integer
+    cout << "Mohamad Adzka Fawaid\n" << endl;
+    /*comment
+    */
+    int x= 2004;
+    int B= 18;
+    float c=176;
+    char f= 'L';
+    string g= "XL" ;
+    cout<< "Tahun lahir :" << x;
+    cout<< "\nUmur saya: " << B;
+    cout<< "\nTinggi : " << c;
+    cout<< "\nJenis kelamin :" << f;
+    cout<< "\nukuran baju :" << g;
+
+    return 0;
+}
