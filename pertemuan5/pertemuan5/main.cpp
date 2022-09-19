ab2f92b14e766dca4d60b7334f4ad9d344e3fe0e
adzkafawaid <112879082+adzkafawaid@users.noreply.github.com>
2022-09-19 21:36:31 +0700

    Add files via upload

---
+++ pertemuan5/pertemuan5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"==========================================================================================="<< endl;
+    cout << "   \t\t\t PERTEMUAN 5 MEMBUAT PROGRAM LUAS LINGKARAN \t\t\t " << endl;
+    cout <<" \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+    cout << "     NAMA : MOHAMAD ADZKA FAWAID , A11202214656 \t\t\t\t\t\t  " << endl;
+    cout <<"   \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+
+    float n,r,hasil;
+    n = 3.14;
+    r = 16;
+    cout <<"\t a. Masukan phi       \t: ";
+    cin >> n;
+    cout <<" \t b. Masukan rusuk  \t: ";
+    cin >> r;
+    cout <<" \t--> Hasilnya adalah \t: " << n*r << endl;
+    cout <<"==========================================================================================="<<endl;
+    return 0;
+}
