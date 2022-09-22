d7c605bb607cb116e565799012f1e2a4c6df3314
adzkafawaid <112879082+adzkafawaid@users.noreply.github.com>
2022-09-22 16:36:30 +0700

    Update main.cpp

--- pertemuan5/pertemuan5/main.cpp
+++ pertemuan5/pertemuan5/main.cpp
@@ -10,14 +10,17 @@ int main()
     cout << "     NAMA : MOHAMAD ADZKA FAWAID , A11202214656 \t\t\t\t\t\t  " << endl;
     cout <<"   \t\t\t\t\t\t\t\t\t\t\t " <<endl;
 
-    float n,r,hasil;
+    float n,r,rumus,hasil;
     n = 3.14;
     r = 16;
+    rumus= n*r*r;
     cout <<"\t a. Masukan phi       \t: ";
     cin >> n;
-    cout <<" \t b. Masukan rusuk  \t: ";
+    cout <<" \t b. Masukan jari-jari \t: ";
     cin >> r;
-    cout <<" \t--> Hasilnya adalah \t: " << n*r << endl;
+    cout <<" \t==> Masukan jari-jari \t: ";
+    cin >> rumus;
+    cout <<" \t--> Hasilnya adalah \t: " << n*r*r << endl;
     cout <<"==========================================================================================="<<endl;
     return 0;
 }
