0e3f4176e8463fbb161328a1d060e4c831147f9a
adzkafawaid <112879082+adzkafawaid@users.noreply.github.com>
2022-10-06 06:42:55 +0700

    Add files via upload

---
+++ pert9/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      // Tugas mencari Kelipatan 5
+    int x;
+    x = 0;
+    do{
+        if(x % 5 == 0)
+        cout << x << endl;
+        x++;
+    }while(x <= 50);
+    return 0;
+    return 0;
+}
